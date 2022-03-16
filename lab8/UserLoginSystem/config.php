dadf3a3bb8a866892ce21c2574d636d0fe81344d
yogeshoyadav08 <yogeshoyadav08@gmail.com>
2022-03-15 22:54:36 -0400

    User Login System Code - Updated config.php file

--- lab8/UserLoginSystem/config.php
+++ lab8/UserLoginSystem/config.php
@@ -3,7 +3,7 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'YY08@mysql');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'demo');
  
 /* Attempt to connect to MySQL database */
